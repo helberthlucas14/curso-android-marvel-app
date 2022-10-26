53dd3b50f986d877239a073345977434b251442b
Helberth Versiani <helberthlucas@ioasys.com.br>
2022-10-26 11:09:29 -0300

    implementa charactersPagingSource

--- core/src/main/java/com/example/core/data/repository/CharactersRepository.kt
+++ core/src/main/java/com/example/core/data/repository/CharactersRepository.kt
@@ -3,7 +3,7 @@ package com.example.core.data.repository
 import androidx.paging.PagingSource
 import com.example.core.domain.model.Character
 
-interface CharacterRespository {
+interface CharactersRepository {
 
     fun getCharacters(query: String): PagingSource<Int, Character>
 }
